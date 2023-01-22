b8f47648202e46d7d4310373f0d6ec089d095e63
Johannes Liem <joh.liem@gmail.com>
2023-01-22 12:00:54 +0100

    update test, add test ouptut to gitignore

--- test/read-xlsx-workbook.test.ts
+++ test/read-xlsx-workbook.test.ts
@@ -16,9 +16,16 @@ test("read xlsx from workbook", () => {
     const importedData = readDataFromXlsxWorkbook(workbook, idPrefix);
     const transformedData = transformData(importedData, idPrefix);
 
-    fs.writeFileSync("test/output/xlsx-as-json.json", JSON.stringify(importedData, null, 4));
+    if (!fs.existsSync("test/output")) {
+        fs.mkdirSync("test/output");
+    }
+    const fNImporedData = "test/output/xlsx-as-json.json";
+    fs.existsSync(fNImporedData) && fs.unlinkSync(fNImporedData);
+    fs.writeFileSync(fNImporedData, JSON.stringify(importedData, null, 4));
 
-    fs.writeFileSync("test/output/data-duerer.json", JSON.stringify(transformedData, null, 4));
+    const fNTransformedData = "test/output/data-duerer.json";
+    fs.existsSync(fNTransformedData) && fs.unlinkSync(fNTransformedData);
+    fs.writeFileSync(fNTransformedData, JSON.stringify(transformedData, null, 4));
 });
 
 test.run();
